cf4ea94bb231f59701d184612c793c63f694cf99
volodymyrlogika <volodymyr.logika@gmail.com>
2025-05-23 05:03:28 +0300

    dist

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { HashRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
-
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
